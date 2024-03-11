556951a7f2fa105003561b3fdc14f74cba2c1a00
Yasin Davids <yasindavids1134@gmail.com>
2024-03-11 09:17:41 +0200

    Add project files.

---
+++ YasinDavids_ST10269809_PROG6221_POE_PART1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YasinDavids_ST10269809_PROG6221_POE_PART1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
